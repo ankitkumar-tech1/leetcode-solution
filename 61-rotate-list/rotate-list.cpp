0fb5becb4cf9e1f6bc61095ee9c9729aac163ddb
Ankit Kumar <ankitkumar2431967@gmail.com>
2026-02-02 15:48:07 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (63.33%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* cur = head;
+        int n = 0;
+        while (cur) {
+            ++n;
+            cur = cur->next;
+        }
+        k %= n;
+        if (k == 0) {
+            return head;
+        }
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (k--) {
+            fast = fast->next;
+        }
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode* ans = slow->next;
+        slow->next = nullptr;
+        fast->next = head;
+        return ans;
+    }
+};
